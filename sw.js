73079171c36fc94b446e3dd640d19f56e2a63bb3
ManlyAndy <ike535.andrey@gmail.com>
2026-08-25 12:31:55 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // интернета". Это специально изменено после случая, когда старая закэшированная
 // версия config.js мешала обновлениям доходить до телефона.
 
-const CACHE_NAME = "sklad-scanner-shell-v9";
+const CACHE_NAME = "sklad-scanner-shell-v9_1";
 const SHELL_FILES = [
   "./index.html",
   "./app.js",
